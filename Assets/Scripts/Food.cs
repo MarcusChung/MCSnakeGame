b5d5fe911c5f6a9d5f158ea4c65fdeb4da1966e2
Marcus Chung <marcuschung2002@gmail.com>
2023-02-22 14:32:04 +0000

    add traps

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(SpriteRenderer))]
 public class Food : MonoBehaviour
 {
+    
     private RandomScriptBehaviour randomFoodGenerator;
     // Start is called before the first frame update
     void Start()
@@ -14,10 +15,21 @@ public class Food : MonoBehaviour
         int x = Random.Range(-17, 20);
         int y = Random.Range(-7, 8);
         transform.position = new Vector2(x, y);
+        //prevent food from spawning on snake
+        foreach (Transform child in transform.parent){
+            if (child.position == transform.position){
+                randomPosition();
+            }
+        }
     }
     private void OnTriggerEnter2D(Collider2D other) {
         randomPosition();
         randomFoodGenerator = FindObjectOfType<RandomScriptBehaviour>();
         randomFoodGenerator.randomSprite();
+    }
+
+     //if position of traps is the same as food, move food
+    private void OnTriggerStay2D(Collider2D other) {
+        randomPosition();
     }
 }
